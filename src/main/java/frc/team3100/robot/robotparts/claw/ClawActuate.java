3fff046217ae1dbc39b8a0a2da3ea3f396b04b9a
aiden7301 <aidenaaj@gmail.com>
2018-08-21 20:55:03 -0500

    Made the claw code look better and slightly more intuitive

--- src/main/java/frc/team3100/robot/robotparts/claw/ClawActuate.java
+++ src/main/java/frc/team3100/robot/robotparts/claw/ClawActuate.java
@@ -5,14 +5,18 @@ import frc.team3100.robot.Robot;
 
 public class ClawActuate extends Command {
 
+    // Since this does not affect any other claw function, we do not have to require all other claw actions to stop.
+    // This avoids a one-tick long stutter in the intake motors.
 
     public ClawActuate(){
         super("ClawActuate");
     }
 
+    private boolean clawOpen = Robot.varLog.clawOpenState;
+
     @Override
     protected void initialize() {
-        if(Robot.varLog.clawOpenState) {
+        if(clawOpen) {
             Robot.claw.close();
             Robot.varLog.clawOpenState = false;
         } else {
